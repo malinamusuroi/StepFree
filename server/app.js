a10dda470ae72673307e812c9570568c68488ee4
malinamusuroi <mmm616@ic.ac.uk>
2018-05-30 18:03:32 +0100

    Create getDirections API

--- server/app.js
+++ server/app.js
@@ -1,11 +1,35 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/getDirections', function (req, res) {
-    console.log("received get directions")
-    res.json({message: 'It is too far'});
-})
+    console.log("received get directions");
+
+    getDirections(req.query.origin, req.query.destination, function (json) {
+        res.json(json)
+    })
+});
+
+const googleMapsClient = require('@google/maps').createClient({
+    key: process.env.WEBAPPS_GMAPS_KEY
+});
+
+function getDirections(origin, destination, callback) {
+    googleMapsClient.directions({
+        origin: origin,
+        destination: destination,
+        mode: 'transit',
+    }, function (err, response) {
+        if (!err) {
+            const json = {
+                origin: origin,
+                destination: destination,
+                legs: response.json.routes[0].legs
+            };
+            callback(json)
+        } else {
+            console.error(err);
+        }
+    });
+}
 
 app.listen(3000);
