918edf02e8799defe70f50ca186abe157a885902
leungrw <leungrw@Rachel-Leung.localdomain>
2018-05-31 12:49:01 +0100

    Added the switch and have everything running in background but not rendering

--- frontend/app/components/Main.js
+++ frontend/app/components/Main.js
@@ -16,12 +16,18 @@ export default class Main extends Component {
     this.state = {
       FROMtext: '',
       TOtext: ''
-    };
+    }
+    //this.onHandleChange = this.onHandleChange.bind(this);
   }
 
 render() {
   return(
+
   <View style={styles.container}>
+
+    <View style={styles.header}>
+      <Text style={styles.headerText}>APP NAME TBD</Text>
+    </View>
 
     <TextInput style={styles.FROMtext}
       placeholder="Enter Start:"
@@ -31,13 +37,17 @@ render() {
       placeholder="Enter Destination:"
       onChangeText = {(TOtext)=>this.setState({TOtext})}/>
 
+    <TouchableOpacity onPress={this.switch.bind(this)} style = {styles.switchButton}>
+      <Text style = {styles.switchButtonText}>↑↓</Text>
+    </TouchableOpacity>
+
     <TouchableOpacity onPress={this.search.bind(this)} style = {styles.findButton}>
       <Text style = {styles.findButtonText}>FIND</Text>
     </TouchableOpacity>
 
-    <ScrollView>
-      <Text style={styles.text}> CHECK </Text>
-      <Text style={styles.text}> CHECK </Text>
+    <ScrollView style={styles.output}>
+      <Text style={styles.text}> HELLO </Text>
+      <Text style={styles.text}> HELLO </Text>
       <Text style={styles.text}> CHECK </Text>
       <Text style={styles.text}> CHECK </Text>
       <Text style={styles.text}> CHECK </Text>
@@ -48,7 +58,7 @@ render() {
       <Text style={styles.text}> CHECK </Text>
     </ScrollView>
 
-    <TouchableOpacity onPress={this.refresh.bind(this)} style = {styles.refreshButton}>
+    <TouchableOpacity onPress={this.refresh.bind(this)} style = {styles.refreshButton} onChange={this.onHandleChange}>
       <Text style = {styles.refreshButtonText}>Clear</Text>
     </TouchableOpacity>
 
@@ -56,11 +66,32 @@ render() {
 )};
 
   search() {
-     alert(this.state.FROMtext + "\n"+ this.state.TOtext);
+    if(this.state.TOtext&&this.state.FROMtext) {
+     //alert(this.state.FROMtext + "\n"+ this.state.TOtext)
+     alert("here")
+    //  fetch('URL', {body : JSON.stringify({
+    //   origin: this.state.FROMtext,
+    //   destination: this.state.TOtext
+    //   })
+    // })
+   } else {
+    alert("Enter start and end destinations");
+   }
+  }
+
+  switch() {
+    if(this.state.TOtext&&this.state.FROMtext){
+      var temp = this.state.FROMtext
+      this.setState({FROMtext: this.state.TOtext}, () => console.log(this.state.FROMtext))
+      this.setState({TOtext: temp},() => console.log(this.state.TOtext))
+    }else {
+      alert("Enter start and end destinations");
+    }
   }
 
   refresh() {
-    alert(this.state.FROMtext + "\n"+ this.state.TOtext);
+    this.setState({FROMtext: ''},() => console.log(this.state.FROMtext))
+    this.setState({TOtext: ''},() => console.log(this.state.TOtext))
   }
 
 }
@@ -70,44 +101,78 @@ const styles = StyleSheet.create({
   container: {
       flex: 1,
   },
+  header: {
+    top: 10,
+    backgroundColor:'#21abcd',
+    alignItems: 'center',
+    justifyContent:'center',
+    height: 80,
+  },
+  headerText: {
+    fontSize: 28,
+    color:'white'
+  },
   FROMtext: {
+    top:10,
     fontSize: 20,
     color: 'black',
-
+    height: 60,
   },
   TOtext: {
     fontSize: 20,
     color: 'black',
-
+    height: 60, 
   },
-  findButton: {
+  switchButtonText:{
+    color: '#fff',
+    fontSize: 28,
+  },
+  switchButton: {
     backgroundColor: '#21abcd',
-    width: 70,
-    height: 70,
-    borderRadius: 30,
+    width: 40,
+    height: 40,
+    borderRadius: 15,
+    start: 10,
     alignItems: 'center',
     justifyContent: 'center',
     alignItems: 'center',
+    alignSelf: 'flex-start',
+  },
+  
+  findButton: {
+    backgroundColor: '#21abcd',
+    width: 70,
+    height: 50,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    //alignItems: 'center',
     alignSelf: 'flex-end',
+    top: -40,
     end: 10,
-    top: 10,
 
   },
   findButtonText: {
     color: '#fff',
     fontSize: 24
   },
+  output:{
+    flex: 1,
+    alignSelf: 'center',
+    top: -30,
+    marginBottom: 10,
+  },
   refreshButton: {
     backgroundColor: '#21abcd',
     width: 70,
-    height: 70,
+    height: 50,
     borderRadius: 30,
-    alignItems: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'flex-end',
+    alignSelf: 'center',
     end: 10,
     bottom: 10,
+    marginTop: -20
   },
   refreshButtonText: {
     color: '#fff',
