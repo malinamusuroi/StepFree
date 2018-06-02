530607a56843ee72a6c30541b15dd526a61ff8ae
Ece Kayan <ecekayan97@gmail.com>
2018-06-02 17:33:21 +0100

    Get lift existence information for tube stations

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const request = require('request');
+const zip = require('zip-array');
 
 app.get('/getDirections', function (req, res) {
 
     getDirections(req.query.origin, req.query.destination, function (json) {
-        res.json(json)
-    })
+        res.json(json);
+    });
+});
+
+getAccessibility(function (body) {
+    console.log(body);
 });
 
 const googleMapsClient = require('@google/maps').createClient({
@@ -30,8 +36,34 @@ function getDirections(origin, destination, callback) {
     });
 }
 
+function getAccessibility(callback) {
+    request("https://tfl.gov.uk/tfl/syndication/feeds/step-free-tube-guide.xml", function (err, response, body) {
+        const parseString = require('xml2js').parseString;
+        parseString(body, function (err, result) {
+            if (!err) {
+                const xml = {
+                    /* Lift existence: Yes/No/Undefined */
+                    liftExistence: getLiftInformation(result.Stations)
+                };
+                callback(JSON.stringify(xml));
+            } else {
+                console.error(err);
+            }
+        });
+    });
+}
+
 function getInstruction(legs) {
     return legs[0].steps.map((step) => step.html_instructions);
+}
+
+function getStationNames(stations) {
+    return stations.Station.map((station) => station.StationName[0]);
+}
+
+function getLiftInformation(stations) {
+    return zip.zip_longest(getStationNames(stations),
+        stations.Station.map((station) => station.Accessibility[0].Lifts[0].AccessViaLift[0]));
 }
 
 app.listen(process.env.PORT || 3000);
