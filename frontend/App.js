69e319ba32f4495cda9ea440a2d9693a260223cb
Rachel Leung <rachel.leung.789@hotmail.com>
2018-06-06 18:00:42 +0100

    Initial implementation of navigation between pages

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,19 @@
-
 import React, { Component } from 'react';
 import Main from './app/components/Main.js'
+import Routes from './app/components/Routes.js'
 
+import { StackNavigator } from 'react-navigation';
+
+const Navigation = StackNavigator ({
+	Front: {screen: Main, navigationOptions:{header: null}},
+	Routes: {screen: Routes},
+	// RouteInfo: {screen: RouteInfo}
+});
 
 export default class App extends Component {
   render() {
     return (
-      <Main />
+      <Navigation />
     );
   }
 }
