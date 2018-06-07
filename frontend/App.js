77f36e6fdd52dbc4cac2068eb60abc393f11ed1b
Rachel Leung <rachel.leung.789@hotmail.com>
2018-06-07 15:45:44 +0100

    Implements navigation for RouteInfo

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Main from './app/components/Main.js'
 import Routes from './app/components/Routes.js'
-
+import RouteInfo from './app/components/RouteInfo.js'
 import { StackNavigator } from 'react-navigation';
 
 const Navigation = StackNavigator ({
 	Front: {screen: Main, navigationOptions:{header: null}},
 	Routes: {screen: Routes},
-	// RouteInfo: {screen: RouteInfo}
+	RouteInfo: {screen: RouteInfo}
 });
 
 export default class App extends Component {
