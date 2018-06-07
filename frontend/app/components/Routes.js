39cc5bf5d14f49a5293749ae5cee8ddca0c73bf0
malinamusuroi <mmm616@ic.ac.uk>
2018-06-07 17:18:29 +0100

    Routes page style changes

--- frontend/app/components/Routes.js
+++ frontend/app/components/Routes.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
 import {
 	Alert,
+  Image,
   StyleSheet,
   Text,
   View,
 	FlatList,
-	TouchableHighlight
+	TouchableOpacity
 } from 'react-native';
 
 export default class Routes extends Component{
+  static navigationOptions = {
+    title: 'Routes'
+  };
+
    _onPressButton() {
   
 	 } 
-	 
+
 	 render() {
 
     const { navigate } =  this.props.navigation;
     const routes = this.props.navigation.state.params.routes
-		return (
-      <View style = {styles.container}>
+		
+   return (
+    <View style = {styles.container}>
+      <Image source = {require('./background2.jpeg')}
+         style = {styles.background}>
+      </Image>
+
 			<FlatList
 				data={routes} 
+        keyExtractor={(r, i) => i + ''}
 				renderItem={({item}) =>  
-			  <TouchableHighlight onPress={()=> navigate('RouteInfo')} underlayColor="white">
+			  <TouchableOpacity onPress={()=> navigate('RouteInfo')} underlayColor="white" style={styles.touchable}>
 					<View style={styles.button}>
             <Text style= {styles.text}>{item}</Text>
           </View> 
-        </TouchableHighlight>
+        </TouchableOpacity>
         }
 			/>
       </View>
@@ -36,20 +47,32 @@ export default class Routes extends Component{
 
 const styles = StyleSheet.create({
   container: {
-	paddingTop: 30,
-	flex: 1,
+	  paddingTop: 30,
+	  flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  background: {
+    flex: 1,
+    width: 415,
+    height: 715,
+    position: 'absolute',
+    resizeMode: 'cover'
+  },
   text: {
-    borderWidth: 1,
-    borderColor: 'black',
+    margin: 8,
     fontSize: 20
   },
   button: {
+    borderWidth: 1,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    borderColor: 'black',
     marginBottom: 30,
-	width: 330,
+	  width: 340,
     alignItems: 'center',
-    backgroundColor: '#2196F3'
-  }
+    backgroundColor: 'white'
+  },
 });
