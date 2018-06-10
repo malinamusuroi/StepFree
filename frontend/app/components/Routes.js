a4d5288105c65b67f0b8a55cb545bf093beed522
malinamusuroi <mmm616@ic.ac.uk>
2018-06-10 11:31:51 +0100

    Refactor route info logic

--- frontend/app/components/Routes.js
+++ frontend/app/components/Routes.js
@@ -29,8 +29,9 @@ export default class Routes extends Component{
 
     const { navigate } =  this.props.navigation;
     const routes = this.props.navigation.state.params.routes;
-	  const routes2 = this.props.navigation.state.params.routes2
-     
+	  const routes2 = this.props.navigation.state.params.routes2;
+    const json = this.props.navigation.state.params.json 
+
    return (
     <View style = {styles.container}>
       <Image source = {require('./background2.jpeg')}
@@ -38,19 +39,59 @@ export default class Routes extends Component{
       </Image>
 
       <FlatList 
-        data={routes} 
+        data={json.routes} 
         keyExtractor={(r, i) => i + ''}
 				renderItem={({item, index}) =>  
 			  <TouchableOpacity onPress={()=> navigate('RouteInfo', {routes2: routes2[index]})} underlayColor="white" style={styles.touchable}>
-					<View style={styles.button}>
-            <Text style= {styles.text}>{item}</Text>
-          </View> 
-         </TouchableOpacity>
+			  <View style={styles.button}>
+           <View>
+              <Text style = {styles.text}>  {this.getSteps(item)} </Text>
+           </View>
+           <Text style = {styles.text}> Duration: {item.duration} </Text>
+           <Text style = {styles.text}> {this.printStepFree(item.accessibility)}</Text>
+        </View> 
+        </TouchableOpacity>
         }
       />
     </View>
-    );
+    );   
+ }
+
+ getSteps(route) {
+    const steps = route.steps
+    const info = this.getInfo(steps)
+    return info;
+ }
+ 
+ getInfo(steps) {
+   return steps.map(step => this.getRoute(step));
+ }
+
+ getRoute(json) {
+   if (json.travelMode === 'WALKING') {
+    return <View> 
+              <Text style = {styles.text}>♿︎{json.durationOfStep} -></Text>
+           </View>
+   } else if (json.travelMode === 'TRANSIT') {
+        if (json.lineDetails.vehicle === 'SUBWAY') {
+          return <View>
+                   <Text style = {styles.text} >🚊 {json.lineDetails.lineType}-></Text>
+                 </View>
+      } else {
+          return <View>
+                   <Text style = {styles.text}>🚌 {json.lineDetails.number} -></Text>
+                 </View>
+      } 
    }
+   return null;
+ }
+
+ printStepFree(info) {
+	 if (info.charAt(0) === 'N') {
+	   return '❌' + info;
+	 }
+	 return '✅'  + info;
+ }
 }
 
 const styles = StyleSheet.create({
@@ -69,7 +110,7 @@ const styles = StyleSheet.create({
   },
   text: {
     marginLeft: 2,
-    margin: 8,
+    margin: 0,
     fontSize: 20,
     textAlign: 'left'
   },
@@ -81,7 +122,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 8,
     borderColor: 'black',
     marginBottom: 10,
-	  width: 340,
+	  width: 360,
     backgroundColor: 'white'
   },
   touchable: {
