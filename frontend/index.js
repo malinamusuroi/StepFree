5a09d5bfe37d1458486d0659f0a9417cac211c20
Ece Kayan <ecekayan97@gmail.com>
2018-06-15 10:47:41 +0100

    Style fixes for accessibility button.

--- frontend/index.js
+++ frontend/index.js
@@ -3,4 +3,8 @@ import App from './App';
 import { YellowBox } from 'react-native';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+//AppRegistry.registerComponent('WebApps_Front_End', () => App);
+
+
+YellowBox.ignoreWarnings(['Warning: encountered 1 time', 'RCTBridge required dispatch_sync to load RCTDevLoadingView.Thismay lead to deadlocks']);
 AppRegistry.registerComponent('WebApps_Front_End', () => App);
