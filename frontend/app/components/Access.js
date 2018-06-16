d2fd9ab167bb59cf783c8f2acba68291e967cfcd
Ece Kayan <ecekayan97@gmail.com>
2018-06-16 15:38:48 +0100

    Fix accessibility info and style rating component

--- frontend/app/components/Access.js
+++ frontend/app/components/Access.js
@@ -1,69 +1,90 @@
 import React, { Component } from 'react';
-import { Rating } from 'react-native-elements';
+import Rating from 'react-native-rating';
+import { Easing } from 'react-native';
 import {
   StyleSheet,
+  TextInput,
   Text,
   View,
-  Image
+  Image,
+  FlatList
 } from 'react-native';
 
+  export default class Access extends Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+    };
+  }
 
-export default class Access extends Component{
+  static navigationOptions = {
+		title: 'Accessibility Details'
+	};
+	
   render() {
     const { navigate } =  this.props.navigation;
     const routes = this.props.navigation.state.params.route
     const steps = routes.steps
-
+    const images = {
+      starFilled: require('./star_filled.png'),
+      starUnfilled: require('./star_unfilled.png')
+    }
+ 
     return(<View style = {styles.container}>
-             <Image source = {require('./background2.jpeg')} style = {styles.image}/>
-               <View style={{backgroundColor: 'white', flexDirection: 'column', alignItems: 'center',
-                             justifyContent: 'center', borderBottomLeftRadius: 8, borderBottomRightRadius: 8,
-                             borderTopLeftRadius: 8, borderTopRightRadius: 8}}>
+             <Image source = {require('./plainbackground.jpg')} style = {styles.image}/>                            
+             <FlatList
+                 data={this.getAccessibilityData(steps)}
+                 scrollEnabled={true}
+                 keyExtractor={(r, i) => i + ''}
+                 renderItem={({item, index}) =>
                  <View style = {styles.view}>
-                   <Text style = {{fontSize: 19}}> {this.getAccessibilityData(steps)} </Text>
-                 </View>
-                 <Rating
-                   showRating
-                   type="custom"
-                   fractions={1}
-                   ratingColor="#21abcd"
-                   ratingBackgroundColor='white'
-                   startingValue={3.6}
-                   imageSize={30}
-                   onFinishRating={this.ratingCompleted}
-                   style={{paddingVertical: 10, margin: 10}}
-                 />
-               </View>
-  	  </View>);
+                   <Text> {item} </Text>
+                 </View>} 
+              />
+              <Rating
+                 onAnimationComplete={rating => this.ratingCompleted(rating)}
+                 selectedStar={images.starFilled}
+                 unselectedStar={images.starUnfilled}
+                 config={{easing: Easing.inOut(Easing.ease), duration: 450 }}
+                 stagger={80}
+                 maxScale={1.4}
+                 starStyle={{width: 40, height: 40}}
+               />
+               <TextInput style={styles.reviewText}
+                  placeholder="Leave a review"
+                  value={this.state.reviewText}
+                  onChangeText = {(reviewText)=>this.setState({reviewText})}/>
+           </View>);
   }
-
+	
   ratingCompleted(rating) {
-    const station1 = "Holborn"
-    fetch('http://localhost:3000/rateStation?station=' + station1 + '&rating=' + rating)
+    const station = encodeURIComponent("Holborn");
+    fetch('https://safe-bastion-98845.herokuapp.com/rateStation?station=' + station + '&rating=' + rating)
+    //fetch('http://localhost:3000/rateStation?station=' + station + '&rating=' + rating)
   }
 
   getAccessibilityData(data) { 
     return data
       .filter(x => x.travelMode === "TRANSIT")
       .map(step => {
-        return step.lineDetails.departureStop + "\n" + this.formatAccess(step.lineDetails.departureAccess) + "\n"
-          step.lineDetails.arrivalStop + "\n" + this.formatAccess(step.lineDetails.arrivalAccess)
-      }).join("\n")
+          return step.lineDetails.departureStop + "\n" + this.formatAccess(step.lineDetails.departureAccess) + "\n"
+                 step.lineDetails.arrivalStop + "\n" + this.formatAccess(step.lineDetails.arrivalAccess)
+    })
   }
 
   formatAccess(data) {
     if (data.length > 0) {
-      return "\tLift: " + data[0].lift + "\n" + this.formatLineInfo(data[0].lineInfo || []) 
+      return "🔹 Lift: " + data[0].lift + "\n" + this.formatLineInfo(data[0].lineInfo || []) 
     } else {
-      return "\t Manual Ramp: Yes"
+      return "🔹 Manual Ramp: Yes"
     }
   }
 
   formatLineInfo(lineInfo) {
     return lineInfo.map(info => {
-      return "\t" + info.lineName + " (" + info.direction + " towards " + info.directionTowards + ")\n\t" +
-      "Step: " + info.stepMin + "–" + info.stepMax + "cm\n\t" +
-      "Gap: " + info.gapMin + "–" + info.gapMax + "cm\n"
+      return "🔹" + info.lineName + " (" + info.direction + " towards " + info.directionTowards .trim()+ ")\n" +
+      "    Step: " + info.stepMin + "–" + info.stepMax + "\n" +
+      "    Gap: " + info.gapMin + "–" + info.gapMax + " cm\n"
     }).join("\n")
   }
 }
@@ -90,6 +111,21 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
     backgroundColor: 'white'
-  }
+  },
+  reviewText: {
+    borderWidth: 1,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: 16,
+    margin: 16,
+    marginTop: 8,
+    marginBottom: 40,
+    fontSize: 16,
+    borderColor: 'black',
+    color: 'black',
+    height: 50,
+  },
 })
  
